1192068260d3727d4711c546096f51be08582c64
Desdinova <florian@infra.net>
2019-03-28 17:26:15 +0100

    create database redditcrawl

--- setup_database.py
+++ setup_database.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
-import MySQLdb
+import MySQLdb as mysql
+
+# Connection to MySQL
+db = mysql.connect(host="localhost",  # your host
+                     user="root",       # username
+                     passwd="")     # password
+
+# Cursor to execute queries
+cur = db.cursor()
+
+# create database "redditcrawl"
+cur.execute("CREATE DATABASE IF NOT EXISTS redditcrawl")
+db.commit()
+
+
 
 
