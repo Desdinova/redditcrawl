c0ba2891b878bdf05000255047df0cfd977c454c
Desdinova <florian@infra.net>
2019-04-02 16:28:45 +0200

    worked on database setup

--- setup_database.py
+++ setup_database.py
@@ -4,19 +4,36 @@ import MySQLdb as mysql
 
 databaseName = "redditcrawl"
 
+
+# Create the database
 # Connection to MySQL
 createDB = mysql.connect(host="localhost",  # your host
                      user="root",       # username
                      passwd="")     # password
 
 # Cursor to create first database
-cur = createDB.cursor()
+curCreateDB = createDB.cursor()
 
-# create database "redditcrawl"
-cur.execute("CREATE DATABASE IF NOT EXISTS " + databaseName)
+# Create database "redditcrawl"
+curCreateDB.execute("CREATE DATABASE IF NOT EXISTS " + databaseName)
+createDB.commit()
+
+
+# Create the tables
+# Connection to MySQL database
+db = mysql.connect(host="localhost",
+		   user="root",
+		   passwd="",
+	   database=databaseName
+	   )
+
+cur = db.cursor()
+
+# create table "submission"
+cur.execute("CREATE TABLE submission (
+	id VARCHAR NOT NULL,
+	subreddit VARCHAR NOT NULL,
+	ups INT
+	)"
+	
 db.commit()
-
-
-
-
-
