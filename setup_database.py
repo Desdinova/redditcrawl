cb9ae86b52ea70bc36a8713471fe17e579dcb0c7
Desdinova <florian@infra.net>
2019-04-03 13:46:52 +0200

    worked on database setup

--- setup_database.py
+++ setup_database.py
@@ -28,6 +28,6 @@ db = mysql.connect(host="localhost",
 cur = db.cursor()
 
 # create table "submission"
-cur.execute("CREATE TABLE submission (id VARCHAR (191) NOT NULL UNIQUE, subreddit VARCHAR (255) NOT NULL, ups INT, _fetched BIT, _flair CHAR, _mod CHAR, approved_at_utc TIMESTAMP)")
+cur.execute("CREATE TABLE submission (id VARCHAR (191) NOT NULL UNIQUE, ups INT, _fetched BIT, _flair CHAR, _mod CHAR, approved_at_utc DATETIME, approved_by TINYTEXT, archived BIT, author TINYTEXT, author_fullname TINYTEXT, banned_at_utc DATETIME, banned_by TINYTEXT, clicked BIT, comment_limit SMALLINT, contest_mode BIT, created DATETIME, created_utc DATETIME, domain TEXT, downs MEDIUMINT, edited BIT, gilded SMALLINT, gildings CHAR, is_crosspostable BIT, is_meta BIT, is_original_content BIT, is_reddit_media_domain BIT, is_self BIT, is_video BIT, likes BIT, locked BIT, media TEXT, media_only BIT, name TINYTEXT, no_follow BIT, num_comments SMALLINT, num_crossposts TINYINT, num_reports TINYINT, over_18 BIT, permalink TEXT, pinned BIT, pwls MEDIUMINT, removal_reason TEXT, report_reasons TEXT, score MEDIUMINT, selftext LONGTEXT, selftext_html LONGTEXT, spoiler BIT, stickied BIT, subreddit TEXT, subreddit_id TINYTEXT, subreddit_name_prefixed TEXT, subreddit_subscribers MEDIUMINT, subreddit_type TINYTEXT, title TEXT, upvote_ratio FLOAT, url TEXT, view_count MEDIUMINT, wls SMALLINT)")
 
 db.commit()
