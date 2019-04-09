72faee47393ffa8cd49419ca1e76c8abb573a643
Desdinova <florian@infra.net>
2019-04-09 12:36:48 +0200

    added mysql datastructure textfile. changed testcrawl.py behaviour

--- testcrawl.py
+++ testcrawl.py
@@ -3,6 +3,7 @@
 import praw
 import json
 import datetime
+import MySQLdb as mysql
 
 #
 # Authentication
@@ -15,7 +16,19 @@ with open("authdata.json") as f:
 reddit = praw.Reddit(client_id=data["client_id"], client_secret=data["client_secret"], password=data["password"], user_agent=data["user_agent"], username=data["username"])
 
 
+# connection to MySQL database
+db = mysql.connect(host="localhost",
+		   user="root",
+		   passwd="", db="redditcrawl")
+
+cur = db.cursor()
+
+
+# print attributes from submissions in linuxadmin
 for post in reddit.subreddit('linuxadmin').new(limit=5):
 	print(post.id)
 	print(post.created)
-	print(post.name)
+	print(post.title)
+	print(\n)
+	# check if entry is present
+	cur.execute("SELECT id FROM submission WHERE id=" + post.id)
